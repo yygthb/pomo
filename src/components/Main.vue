c7ec3b01cf5456a4aa9cfb989b9d5f8857f2b4f2
Yura Ya <yura@revpush.cc>
2024-09-05 12:47:09 +0300

    feat: timer and the bell sound

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,73 @@
-<script>
+<script setup>
+import { computed, ref } from "vue";
+import AppButton from "./ui/AppButton.vue";
+import BellSound from '../assets/audio/bell.mp3';
+
+const pomodoroTimerValue = 25 * 60;
+
+const timer = ref(pomodoroTimerValue);
+var timerInterval;
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    if (timer.value > 0) {
+      timer.value--;
+    } else {
+      clearInterval(timerInterval);
+      ringTheBell(BellSound);
+      return;
+    }
+  }, 1000);
+}
+
+function pauseTimer() {
+  clearInterval(timerInterval);
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  timer.value = pomodoroTimerValue;
+}
+
+const min = computed(() => {
+  return Math.floor(timer.value / 60);
+});
+
+const sec = computed(() => {
+  return timer.value - min.value * 60;
+});
+
+function ringTheBell(sound) {
+  let note = new Audio(sound);
+  note.addEventListener("canplaythrough", () => { 
+    note.play();
+  });
+}
+
+function setTimerToValue(val) {
+  timer.value = val;
+}
 </script>
 
 <template>
   <main class="main">
-    <div class="container">MAIN</div>
+    <div class="container">
+      <div class="pomodoro-container">
+        <div class="timer">
+          <div>
+            {{ timer }}
+          </div>
+          <div>{{ min < 10 ? `0${min}` : min }} : {{ sec < 10 ? `0${sec}` : sec }}</div>
+        </div>
+        <div class="controls">
+          <AppButton @click="startTimer">Start</AppButton>
+          <AppButton @click="pauseTimer">Pause</AppButton>
+          <AppButton @click="resetTimer">Reset</AppButton>
+        </div>
+
+        <!-- <AppButton :text="'Set timer to 10sec'" @click="setTimerToValue(10)" /> -->
+      </div>
+    </div>
   </main>
 </template>
 
---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  text: String,
+});
+</script>
+
+<template>
+  <button v-if="text" v-text="text"></button>
+  <button v-else>
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  border: 1px solid red;
+}
+</style>
