39e8cd3608343bead930647d68ec4974e6b8f5a7
Yura Ya <yura@revpush.cc>
2024-09-07 21:11:27 +0300

    feat: range ui component

---
+++ src/components/ui/AppRange.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    default: 50,
+  },
+  changeHandler: {
+    type: Function,
+  }
+})
+
+const rangeEmit = defineEmits(['update:modelValue'])
+
+function changeHandler(e) {
+  rangeEmit('update:modelValue', e.target.value)
+  props.changeHandler();
+}
+</script>
+
+<template>
+  <input type="range" :value="modelValue" @input="changeHandler" step="10" />
+</template>
+
+<style lang="scss" scoped>
+</style>
