bce02cb2da77a9837fefcab2a26cb43492e32f32
Yura Ya <yura@revpush.cc>
2024-09-08 13:46:38 +0300

    feat: InputNumber ui component

---
+++ src/components/ui/InputNumber.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  min: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const value = defineModel();
+const emit = defineEmits(["onInput"]);
+
+function onInput(e) {
+  emit("onInput", +e.target.value);
+}
+</script>
+
+<template>
+  <input
+    type="number"
+    :value="value"
+    :min="min"
+    step="1"
+    class="input"
+    @input="onInput"
+  />
+</template>
+
+<style lang="scss" scoped>
+.input {
+  padding: 5px 10px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  font-size: 24px;
+  outline: none;
+  width: 150px;
+}
+</style>
